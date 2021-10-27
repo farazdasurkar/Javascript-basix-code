c4b144b7eab4db5e87d6862187d15541c67af14a
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-10-27 23:52:05 +0530

    commit

--- home.js
+++ home.js
@@ -1,6 +1,10 @@
 console.log('hello')
+
 alert('Warning, U r learning Java script')
+
 var b = 'hello'
 console.log(b)
 
 document.getElementById("fromindex").innerHTML = "I am from HoMe.js"
+
+prompt('what is your age')
