206ba3f7eee6b4c7dede0a86e902f2acdd042ca8
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-10-29 18:40:42 +0530

    commit

--- home.js
+++ home.js
@@ -1,13 +1,17 @@
-console.log('hello')
+// console.log('hello')
 
-alert('Warning, U r learning Java script')
+// alert('Warning, U r learning Java script')
 
-var b = 'hello'
-console.log(b)
+// var b = 'hello'
+// console.log(b)
 
-document.getElementById("fromindex").innerHTML = "I am from HoMe.js"
+// document.getElementById("fromindex").innerHTML = "I am from HoMe.js"
 
 
-var jsage = prompt('what is your age')
-document.getElementById("fromindex").innerHTML = jsage
+// var jsage = prompt('what is your age')
+// document.getElementById("fromindex").innerHTML = jsage
 
+var Intlliscore = Math.random();
+Intlliscore = Intlliscore * 6
+Intlliscore = Math.floor(Intlliscore);
+console.log(Intlliscore);
