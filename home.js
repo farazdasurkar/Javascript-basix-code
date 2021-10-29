ffe8d8fb56ad02d23143c3ebf45d7f80601bbfc7
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-10-29 18:47:11 +0530

    commit

--- home.js
+++ home.js
@@ -11,7 +11,12 @@
 // var jsage = prompt('what is your age')
 // document.getElementById("fromindex").innerHTML = jsage
 
-var Intlliscore = Math.random();
-Intlliscore = Intlliscore * 6
-Intlliscore = Math.floor(Intlliscore);
-console.log(Intlliscore);
+// var Intlliscore = Math.random();
+// Intlliscore = Intlliscore * 6;
+// Intlliscore = Math.floor(Intlliscore);
+// console.log(Intlliscore);
+
+prompt("Whats Is Your Name: ")
+var Intlliscore = Math.random() * 100;
+Intlliscore = Math.floor(Intlliscore) + 1;
+alert("Your Intelegent scrore is " + Intlliscore + "%")
